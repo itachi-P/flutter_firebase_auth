263c4292f9f45db750b011f607f854427d791ca5
itachi-P <itachip38@gmail.com>
2020-03-12 22:29:46 +0900

    Value passing sample 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'second_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,70 +12,130 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FirstPage(title: 'Navigator / async & await Demo'),
+      home: FirstPage(),
     );
   }
 }
 
-class FirstPage extends StatefulWidget {
-  FirstPage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _FirstPageState createState() => _FirstPageState();
-}
-
-class _FirstPageState extends State<FirstPage> {
-  String _result = "初期表示";
+class FirstPage extends StatelessWidget {
+  FirstPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('First Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              child: Text("次の画面へ"),
-              onPressed: () async {
-                String result = await Navigator.of(context).push(
-                  MaterialPageRoute<String>(
-                    builder: (context) => NextPage(),
-                  ),
-                );
-                setState(() => _result = result);
-              },
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.topCenter,
+            padding:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height / 4),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                _buildContents(
+                  [
+                    Text('Pattern 1'),
+                    RaisedButton(
+                      child: Text('Next'),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return SecondPage();
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                _buildContents(
+                  [
+                    Text('Pattern 2'),
+                    RaisedButton(
+                      child: Text('Next'),
+                      onPressed: () async {
+                        await Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return SecondPage();
+                            },
+                          ),
+                        );
+                        showDialog(
+                          context: context,
+                          builder: (context) => SampleDialog(),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                _buildContents(
+                  [
+                    Text('Pattern 3'),
+                    RaisedButton(
+                      child: Text('Next'),
+                      onPressed: () async {
+                        final result = await Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return SecondTextInputPage();
+                            },
+                          ),
+                        );
+                        if (result != null) {
+                          final contentText = 'I received ' + result + ' !';
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return SampleDialog(
+                                contentText: contentText,
+                              );
+                            },
+                          );
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Text(_result)
-          ],
+          ),
         ),
       ),
     );
   }
 }
 
-class NextPage extends StatelessWidget {
+Widget _buildContents(List<Widget> children) {
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 40.0),
+    child: Column(
+      children: children,
+    ),
+  );
+}
+
+class SampleDialog extends StatelessWidget {
+  const SampleDialog({Key key, this.contentText}) : super(key: key);
+
+  final String contentText;
+
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () {
-        Navigator.of(context).pop("戻るアイコンからの遷移");
-        return Future.value(false);
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("NextPage"),
-        ),
-        body: Center(
-          child: RaisedButton(
-            child: Text("戻る"),
-            onPressed: () => Navigator.of(context).pop("戻るボタンからの遷移"),
-          ),
-        ),
+    final content = contentText ?? 'I\'m back!';
+    return AlertDialog(
+      title: Text(
+        'Alert',
+        style: Theme.of(context).textTheme.title,
+      ),
+      content: Text(
+        content,
+        maxLines: 2,
+        style: Theme.of(context).textTheme.body2,
       ),
     );
   }
